d375e78cb03aef0ca0ff7b4e29c825a51ec37ae2
QUAFFquaff <632633533@qq.com>
2019-05-15 20:58:50 -0700

    little change

--- OBD_project-master/dataHandler/change_numbers_to_alphabet.py
+++ OBD_project-master/dataHandler/change_numbers_to_alphabet.py
@@ -25,52 +25,51 @@ def change_number_label_to_alphabet():
     write_excel(np.array(datamatrix), 'ForLDA0415Alph.xls')
 
 
+def change_n_to_a(temp):
+    if temp == 1:
+        temp = "a"
+    elif temp == 5:
+        temp = "b"
+    elif temp == 9:
+        temp = "c"
+    elif temp == 0:
+        temp = "h"
+    elif temp == 4:
+        temp = "i"
+    elif temp == 8:
+        temp = "j"
+    elif temp == 2:
+        temp = "o"
+    elif temp == 6:
+        temp = "p"
+    elif temp == 10:
+        temp = "q"
+    elif temp == 3:
+        temp = "v"
+    elif temp == 7:
+        temp = "w"
+    elif temp == 11:
+        temp = "x"
+    return temp
+
+
 def read_excel(file):
-    time_window = 40 * 1000
     data = xlrd.open_workbook(file)
     table = data.sheets()[0]
 
-    start = 0  # 开始的行
     list_values = []
     end = len(table.col_values(0))  # from the first line to last line
-    for x in range(start, end):
+    for x in range(end):
         values = []
         row = table.row_values(x)
-        # orientation X,Y; acceleration X,Y, SMA X,Y; time
         for i in [0, 1, 2]:
-            # print(value)
             values.append(row[i])
-        if values[2] == 1:
-            values[2] = "a"
-        elif values[2] == 5:
-            values[2] = "b"
-        elif values[2] == 9:
-            values[2] = "c"
-        elif values[2] == 0:
-            values[2] = "h"
-        elif values[2] == 4:
-            values[2] = "i"
-        elif values[2] == 8:
-            values[2] = "j"
-        elif values[2] == 2:
-            values[2] = "o"
-        elif values[2] == 6:
-            values[2] = "p"
-        elif values[2] == 10:
-            values[2] = "q"
-        elif values[2] == 3:
-            values[2] = "v"
-        elif values[2] == 7:
-            values[2] = "w"
-        elif values[2] == 11:
-            values[2] = "x"
+        values[2] = change_n_to_a(values[2])
         list_values.append(values)
-    # print(list_values)
     return list_values
 
 
 def write_data(dataTemp, table):
-    # print(dataTemp)
     [h, l] = dataTemp.shape  # h为行数，l为列数
     for i in range(h):
         for j in range(l):
