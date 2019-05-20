2557f0bfaad221b0059853991705200540c17f43
Daben <wdb747534116@outlook.com>
2019-05-20 13:06:25 -0700

    change test of bluetooth

--- OBD_project-master/OBD_GUI/test.py
+++ OBD_project-master/OBD_GUI/test.py
@@ -4,6 +4,8 @@ ser = serial.Serial(port='/dev/rfcomm0',baudrate=57600,timeout = 0.5)
 print(ser)
 data = ''
 data = data.encode('utf-8')
+if !ser.is_open:
+    ser.open()
 n = ser.inWaiting()
 data = data+ser.readline()
 print('get data:',data)
