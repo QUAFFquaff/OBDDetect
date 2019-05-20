4c3539ebe2ce0efcdec837be93aa6349ba571d48
Daben <wdb747534116@outlook.com>
2019-05-20 13:28:30 -0700

    succeed to get bluetooth data from serial by python

--- OBD_project-master/OBD_GUI/test.py
+++ OBD_project-master/OBD_GUI/test.py
@@ -1,12 +1,17 @@
 import serial
+import time
 
 ser = serial.Serial(port='/dev/rfcomm0',baudrate=57600,timeout = 0.5)
 print(ser)
 data = ''
 data = data.encode('utf-8')
-if !ser.is_open:
+if not ser.is_open:
     ser.open()
 n = ser.inWaiting()
-data = data+ser.readline()
-print('get data:',data)
-print(type(data))
+print('byte num:',str(n))
+while True:
+    obddata = data+ser.readline()
+    if obddata!=b'':
+        print('get data:',obddata)
+        millis = int(round(time.time()*1000))
+        print(millis)
