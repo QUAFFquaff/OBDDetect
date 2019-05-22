ab35bebcd21740276802b8a84a5bc2eec8d87e03
Daben <wdb747534116@outlook.com>
2019-05-21 19:29:50 -0700

    prapare for testing

--- OBD_project-master/OBD_GUI/test.py
+++ OBD_project-master/OBD_GUI/test.py
@@ -1,26 +1,18 @@
-# import serial
-# import time
-#
-# ser = serial.Serial(port='/dev/rfcomm0',baudrate=57600,timeout = 0.5)
-# print(ser)
-# data = ''
-# data = data.encode('utf-8')
-# if not ser.is_open:
-#     ser.open()
-# n = ser.inWaiting()
-# print('byte num:',str(n))
-# while True:
-#     obddata = data+ser.readline()
-#     if obddata!=b'':
-#         print('get data:',obddata)
-#         millis = int(round(time.time()*1000))
-#         print(millis)
+import serial
+import time
 
-obdData = b"sds,0,3.4\r\n"
-row = obdData.split(b"\r")[0]
-row = row.split(b",")
-newrow = []
-newrow.append(str(row[0],encoding="utf-8"))
-newrow.append(int(str(row[1],encoding="utf-8")))
-newrow.append(float(str(row[2],encoding="utf-8")))
-print(newrow)
+ser = serial.Serial(port='/dev/rfcomm0',baudrate=57600,timeout = 0.5)
+print(ser)
+data = ''
+data = data.encode('utf-8')
+if not ser.is_open:
+    ser.open()
+n = ser.inWaiting()
+print('byte num:',str(n))
+while True:
+    obddata = data+ser.readline()
+    if obddata!=b'':
+        print('get data:',obddata)
+        millis = int(round(time.time()*1000))
+        print(millis)
+
