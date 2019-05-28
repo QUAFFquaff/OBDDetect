898725e2ceb212a232b91ebe6573fd32eef3a40f
DabenW <wdb747534116@outlook.com>
2019-05-28 13:42:23 -0700

    e

--- OBD_project-master/OBD_GUI/EventDetect.py
+++ OBD_project-master/OBD_GUI/EventDetect.py
@@ -209,7 +209,7 @@ class detectProcess(multiprocessing.Process):  # threading.Thread
                         [timestamp, speed, accysf[-4], accxsf[-4], acczsf[-4], gyox, gyoy, gyoz])
 
                     # start a thread to store data into databse
-                    dataQueue.put([[device,speed, accysf[-4], accxsf[-4], acczsf[-4], gyox, gyoy, gyoz], timestamp])
+                    dataQueue.put([np.array[device,speed, accysf[-4], accxsf[-4], acczsf[-4], gyox, gyoy, gyoz], timestamp])
 
                     # put the event into Queue
                     if not event is None:
