0189fc918e29ac49b1c148128b9208da90a72d40
QUAFFquaff <632633533@qq.com>
2019-06-03 11:39:03 -0700

    add logger"

--- OBD_project-master/OBD_GUI/EventDetect.py
+++ OBD_project-master/OBD_GUI/EventDetect.py
@@ -13,6 +13,17 @@ import sys
 import multiprocessing
 from ctypes import c_bool
 
+import logging .config
+import logging
+
+logging.basicConfig(level=logging.INFO,#控制台打印的日志级别
+                    filename='new.log',
+                    filemode='a',##模式，有w和a，w就是写模式，每次都会重新写日志，覆盖之前的日志
+                    #a是追加模式，默认如果不写的话，就是追加模式
+                    format=
+                    '%(asctime)s - %(pathname)s[line:%(lineno)d] - %(levelname)s: %(message)s'
+                    #日志格式
+                    )
 import concurrent.futures
 
 sys.path.append('../dataHandler/')
@@ -55,6 +66,33 @@ SVMResultQueue = queue.Queue()
 xstdQueue = queue.Queue(maxsize=19)
 ystdQueue = queue.Queue(maxsize=19)
 
+class Logger(object):
+    level_relations = {
+        'debug':logging.DEBUG,
+        'info':logging.INFO,
+        'warning':logging.WARNING,
+        'error':logging.ERROR,
+        'crit':logging.CRITICAL
+    }#日志级别关系映射
+
+    def __init__(self,filename,level='info',when='D',backCount=3,fmt='%(asctime)s - %(pathname)s[line:%(lineno)d] - %(levelname)s: %(message)s'):
+        self.logger = logging.getLogger(filename)
+        format_str = logging.Formatter(fmt)#设置日志格式
+        self.logger.setLevel(self.level_relations.get(level))#设置日志级别
+        sh = logging.StreamHandler()#往屏幕上输出
+        sh.setFormatter(format_str) #设置屏幕上显示的格式
+        th = handlers.TimedRotatingFileHandler(filename=filename,when=when,backupCount=backCount,encoding='utf-8')#往文件里写入#指定间隔时间自动生成文件的处理器
+        #实例化TimedRotatingFileHandler
+        #interval是时间间隔，backupCount是备份文件的个数，如果超过这个个数，就会自动删除，when是间隔的时间单位，单位有以下几种：
+        # S 秒
+        # M 分
+        # H 小时、
+        # D 天、
+        # W 每星期（interval==0时代表星期一）
+        # midnight 每天凌晨
+        th.setFormatter(format_str)#设置文件里写入的格式
+        self.logger.addHandler(sh) #把对象加到logger里
+        self.logger.addHandler(th)
 
 def getSerial():
     ser = serial.Serial(port='/dev/rfcomm0', baudrate=57600, timeout=0.5)
@@ -529,6 +567,10 @@ class Thread_for_lda(threading.Thread):  # threading.Thread
                 print("temp word       :   " + temp_word)
                 print("time window size:   " + str(time.time() - start_time))
                 print("__________________")
+                logging.INFO("__________________")
+                logging.INFO("temp word       :   " + temp_word)
+                logging.INFO("time window size:   " + str(time.time() - start_time))
+                logging.INFO("__________________")
                 start_time = time.time()
                 trip_svm_buffer += temp_word
                 svm_label_buffer = ""
--- OBD_project-master/dataHandler/makeData.py
+++ OBD_project-master/dataHandler/makeData.py
@@ -16,7 +16,56 @@
 
 '''
 import random
+import logging .config
+import logging
+import logging
+from logging import handlers
 
+class Logger(object):
+    level_relations = {
+        'debug':logging.DEBUG,
+        'info':logging.INFO,
+        'warning':logging.WARNING,
+        'error':logging.ERROR,
+        'crit':logging.CRITICAL
+    }#日志级别关系映射
+
+    def __init__(self,filename,level='info',when='D',backCount=3,fmt='%(asctime)s - %(pathname)s[line:%(lineno)d] - %(levelname)s: %(message)s'):
+        self.logger = logging.getLogger(filename)
+        format_str = logging.Formatter(fmt)#设置日志格式
+        self.logger.setLevel(self.level_relations.get(level))#设置日志级别
+        sh = logging.StreamHandler()#往屏幕上输出
+        sh.setFormatter(format_str) #设置屏幕上显示的格式
+        th = handlers.TimedRotatingFileHandler(filename=filename,when=when,backupCount=backCount,encoding='utf-8')#往文件里写入#指定间隔时间自动生成文件的处理器
+        #实例化TimedRotatingFileHandler
+        #interval是时间间隔，backupCount是备份文件的个数，如果超过这个个数，就会自动删除，when是间隔的时间单位，单位有以下几种：
+        # S 秒
+        # M 分
+        # H 小时、
+        # D 天、
+        # W 每星期（interval==0时代表星期一）
+        # midnight 每天凌晨
+        th.setFormatter(format_str)#设置文件里写入的格式
+        self.logger.addHandler(sh) #把对象加到logger里
+        self.logger.addHandler(th)
+
+def test_log():
+    log = Logger('all.log',level='debug')
+    log.logger.debug('debug')
+    log.logger.info('info')
+    log.logger.warning('警告')
+    log.logger.error('报错')
+    log.logger.critical('严重')
+    Logger('error.log', level='error').logger.error('error')
+
+logging.basicConfig(level=logging.INFO,#控制台打印的日志级别
+                    filename='new.log',
+                    filemode='a',##模式，有w和a，w就是写模式，每次都会重新写日志，覆盖之前的日志
+                    #a是追加模式，默认如果不写的话，就是追加模式
+                    format=
+                    '%(asctime)s - %(pathname)s[line:%(lineno)d] - %(levelname)s: %(message)s'
+                    #日志格式
+                    )
 notebook = "ahovbipwcjqx"
 
 
@@ -104,12 +153,14 @@ def makeDocument(level):
 
 def main():
     data = ""
+
+    log = Logger('all.log',level='debug')
     for j in range(4):
         for i in range(50):
             data += makeDocument(j) + ",\n"
     write(data)
     data1 = readText()
-    print(readText())
+    log.logger.info("test")
 
 
 def write(data):
@@ -125,7 +176,8 @@ def readText():
     try:
         f = open('fakeData.txt', 'r')  # 打开文件
         data = f.read()  # 读取文件内容
-        print(data)
+        # print(data)
+        logging.info(data)
         # return data[0]
     finally:
         if f:
